293c1a3949dbe4722b66ef52bf45ab51d91435a7
Paul Schwarzenberger <paul@celidor.net>
2020-03-03 10:30:34 +0000

    KMS module

---
+++ kms/main.tf
@@ -0,0 +1,15 @@
+resource "aws_kms_key" "encryption" {
+  description         = "KMS key for encryption within ${var.environment} environment"
+  enable_key_rotation = true
+  tags = merge(
+  var.common_tags,
+  map(
+  "Name", "${var.project}-${var.function}-${var.environment}"
+  )
+  )
+}
+
+resource "aws_kms_alias" "encryption" {
+  name          = "alias/${var.project}-${var.function}-${var.environment}"
+  target_key_id = aws_kms_key.encryption.key_id
+}
---
+++ kms/variables.tf
@@ -0,0 +1,15 @@
+variable "common_tags" {
+  description = "tags used across the project"
+}
+
+variable "project" {
+  description = "abbreviation for the project, e.g. tdr, forms the first part of the bucket name"
+}
+
+variable "function" {
+  description = "forms the second part of the bucket name, eg. upload"
+}
+
+variable "environment" {
+  description = "environment, e.g. prod"
+}
