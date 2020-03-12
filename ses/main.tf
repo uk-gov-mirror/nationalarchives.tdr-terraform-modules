987e5f221e8e260358c4b163ce9ee8d210133272
Paul Schwarzenberger <paul@celidor.net>
2020-03-12 10:51:28 +0000

    SES DKIM

--- ses/main.tf
+++ ses/main.tf
@@ -1,10 +1,10 @@
 resource "aws_ses_domain_identity" "email_domain" {
-  domain = var.environment_full_name == "production" ? "${var.project}.${var.domain}" : "${var.project}-${var.environment_full_name}.${var.domain}"
+  domain = local.domain
 }
 
 resource "aws_route53_record" "amazonses_verification_record" {
   zone_id = var.hosted_zone_id
-  name    = var.environment_full_name == "production" ? "_amazonses.${var.project}.${var.domain}" : "_amazonses.${var.project}-${var.environment_full_name}.${var.domain}"
+  name    = "_amazonses.${local.domain}"
   type    = "TXT"
   ttl     = "600"
   records = [aws_ses_domain_identity.email_domain.verification_token]
@@ -16,6 +16,19 @@ resource "aws_ses_domain_identity_verification" "email_verification" {
   depends_on = [aws_route53_record.amazonses_verification_record]
 }
 
+resource "aws_ses_domain_dkim" "email_dkim" {
+  domain = aws_ses_domain_identity.email_domain.domain
+}
+
+resource "aws_route53_record" "amazonses_dkim_record" {
+  count   = 3
+  zone_id = var.hosted_zone_id
+  name    = "${element(aws_ses_domain_dkim.email_dkim.dkim_tokens, count.index)}._domainkey.${local.domain}"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.email_dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
 resource "aws_ses_email_identity" "email_address" {
-  email = var.environment_full_name == "production" ? "${var.email_address}@${var.project}.${var.domain}" : "${var.email_address}@${var.project}-${var.environment_full_name}.${var.domain}"
+  email = "${var.email_address}@${var.domain}"
 }
