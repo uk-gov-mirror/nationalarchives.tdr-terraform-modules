7167d2588f5fa1744fb7fef2e770f35ba3fd127a
Paul Schwarzenberger <paul@celidor.net>
2020-03-13 11:07:39 +0000

    Attach support role to group for CIS compliance

--- iam/variables.tf
+++ iam/variables.tf
@@ -2,3 +2,8 @@ variable "aws_account_level" {
   description = "set to true if configuring at the AWS account level"
   default     = false
 }
+
+variable "support_group" {
+  description = "group giving permissions to manage support calls with AWS"
+  default     = "support"
+}
