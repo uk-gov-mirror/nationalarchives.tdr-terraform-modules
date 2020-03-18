34b04be21edebe0aab1bb03b5fd88e1ae0ba8b63
Paul Schwarzenberger <paul@celidor.net>
2020-03-18 10:42:24 +0000

    hosted zone DNS delegation

--- ses/main.tf
+++ ses/main.tf
@@ -26,12 +26,12 @@ resource "aws_ses_domain_dkim" "email_dkim" {
 
 # include environment in conditional statement until DNS delegation is in place
 resource "aws_route53_record" "amazonses_dkim_record" {
-  count  = var.environment_full_name == "production" ? 0 : 3
+  count   = var.environment_full_name == "production" ? 0 : 3
   zone_id = var.hosted_zone_id
-  name    = "${element(aws_ses_domain_dkim.email_dkim.*.dkim_tokens[0],count.index)}._domainkey.${local.domain}"
+  name    = "${element(aws_ses_domain_dkim.email_dkim.*.dkim_tokens[0], count.index)}._domainkey.${local.domain}"
   type    = "CNAME"
   ttl     = "600"
-  records = ["${element(aws_ses_domain_dkim.email_dkim.*.dkim_tokens[0],count.index)}.dkim.amazonses.com"]
+  records = ["${element(aws_ses_domain_dkim.email_dkim.*.dkim_tokens[0], count.index)}.dkim.amazonses.com"]
 }
 
 resource "aws_ses_email_identity" "email_address" {
