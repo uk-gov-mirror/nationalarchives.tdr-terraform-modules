9494a2d36478846135305aadde7cbe91b19e7604
Paul Schwarzenberger <paul@celidor.net>
2020-04-29 09:46:46 +0100

    increase options and use templatefile function

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,15 +1,15 @@
 output "s3_bucket_id" {
-  value = aws_s3_bucket.bucket.id
+  value = aws_s3_bucket.bucket.*.id[0]
 }
 
 output "s3_bucket_arn" {
-  value = aws_s3_bucket.bucket.arn
+  value = aws_s3_bucket.bucket.*.arn[0]
 }
 
 output "s3_bucket_domain_name" {
-  value = aws_s3_bucket.bucket.bucket_domain_name
+  value = aws_s3_bucket.bucket.*.bucket_domain_name[0]
 }
 
 output "s3_bucket_regional_domain_name" {
-  value = aws_s3_bucket.bucket.bucket_regional_domain_name
+  value = aws_s3_bucket.bucket.*.bucket_regional_domain_name[0]
 }
