c2bbc082768c94f8f5c5bc45fe869345cac5a0dd
Paul Schwarzenberger <paul@celidor.net>
2020-05-12 09:09:54 +0100

    updated resource block name to be generic

--- sns/main.tf
+++ sns/main.tf
@@ -1,4 +1,4 @@
-resource "aws_sns_topic" "log_aggregation" {
+resource "aws_sns_topic" "sns_topic" {
   count  = var.apply_resource == true ? 1 : 0
   name   = local.sns_topic_name
   policy = templatefile("./tdr-terraform-modules/sns/templates/${var.sns_policy}.json.tpl", { region = var.region, account_id = data.aws_caller_identity.current.account_id, sns_topic_name = local.sns_topic_name })
