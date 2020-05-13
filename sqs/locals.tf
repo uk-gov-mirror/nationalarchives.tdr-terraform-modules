63008fce028e2e62a6350e5070db70905087100a
TomJKing <tom.king@nationalarchives.gov.uk>
2020-05-13 07:37:55 +0100

    Add 'queue' to sqs name

--- sqs/locals.tf
+++ sqs/locals.tf
@@ -1,5 +1,5 @@
 locals {
   workspace   = lower(terraform.workspace)
   environment = local.workspace == "default" ? "mgmt" : local.workspace
-  sqs_name    = "${var.project}-${var.function}-${local.environment}"
+  sqs_name    = "${var.project}-${var.function}-queue-${local.environment}"
 }
