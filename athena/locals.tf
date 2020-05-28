bb42dcb1b95e934d94094a1551883d757d01952f
Paul Schwarzenberger <paul@celidor.net>
2020-05-28 12:19:19 +0100

    indentation

--- athena/locals.tf
+++ athena/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  workspace            = lower(terraform.workspace)
-  environment          = local.workspace == "default" ? "mgmt" : local.workspace
+  workspace   = lower(terraform.workspace)
+  environment = local.workspace == "default" ? "mgmt" : local.workspace
   athena_name = "${var.project}_${var.function}_${local.environment}"
 }
