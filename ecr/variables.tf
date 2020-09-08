6d2c66c0e3f18580509c8aa298ef37e7fbfe5c71
sam <github@sampalmer.dev>
2020-09-08 13:49:30 +0100

    Add policy for ECR repositories

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -2,3 +2,12 @@ variable "tag_mutability" {
   default = "MUTABLE"
 }
 variable "name" {}
+
+variable "policy_name" {
+  default = ""
+}
+
+variable "policy_variables" {
+  type    = map
+  default = {}
+}
