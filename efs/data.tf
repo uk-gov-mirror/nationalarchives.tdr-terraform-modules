a6031ce18757f9047c6e95654493e25d10029141
sam <github@sampalmer.dev>
2020-09-08 13:42:44 +0100

    Move EFS resources to efs module.
    These resources were originally only used by the file format lambda so
    they were in that file but they need to be shared now so I've moved them
    to the efs module where they should have been anyway.

--- efs/data.tf
+++ efs/data.tf
@@ -2,3 +2,18 @@ data "aws_caller_identity" "current" {}
 data "aws_kms_key" "efs_kms_key" {
   key_id = "alias/aws/elasticfilesystem"
 }
+data "aws_vpc" "current" {
+  tags = {
+    Name = "${var.project}-vpc-${local.environment}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_nat_gateway" "main_zero" {
+  tags = map("Name", "nat-gateway-0-tdr-${local.environment}")
+}
+
+data "aws_nat_gateway" "main_one" {
+  tags = map("Name", "nat-gateway-1-tdr-${local.environment}")
+}
--- lambda/file_format.tf
+++ lambda/file_format.tf
@@ -10,13 +10,9 @@ resource "aws_lambda_function" "file_format_lambda_function" {
   tags          = var.common_tags
   environment {
     variables = {
-      ENVIRONMENT   = local.environment
-      INPUT_QUEUE   = local.file_format_queue_url
-      OUTPUT_QUEUE  = local.api_update_queue_url
-      AUTH_URL      = var.auth_url
-      API_URL       = "${var.api_url}/graphql"
-      CLIENT_ID     = "tdr-backend-checks"
-      CLIENT_SECRET = data.aws_ssm_parameter.backend_checks_client_secret[0].value
+      ENVIRONMENT  = local.environment
+      INPUT_QUEUE  = local.file_format_queue_url
+      OUTPUT_QUEUE = local.api_update_queue_url
     }
   }
   file_system_config {
@@ -25,95 +21,14 @@ resource "aws_lambda_function" "file_format_lambda_function" {
     local_mount_path = "/mnt/fileformat"
   }
 
-  depends_on = [aws_efs_mount_target.target_az_zero, aws_efs_mount_target.target_az_one]
-
   vpc_config {
-    subnet_ids         = aws_subnet.file_format_private.*.id
-    security_group_ids = [aws_security_group.allow_efs_lambda[count.index].id]
+    subnet_ids         = [data.aws_subnet.efs_private_subnet_zero.id, data.aws_subnet.efs_private_subnet_one.id]
+    security_group_ids = [data.aws_security_group.efs_lambda_security_group.id]
   }
 
   lifecycle {
     ignore_changes = [filename]
   }
-}
-
-resource "aws_efs_mount_target" "target_az_zero" {
-  count           = local.count_file_format
-  file_system_id  = var.file_system.id
-  subnet_id       = aws_subnet.file_format_private[0].id
-  security_groups = [aws_security_group.mount_target_sg[0].id]
-}
-
-resource "aws_efs_mount_target" "target_az_one" {
-  count           = local.count_file_format
-  file_system_id  = var.file_system.id
-  subnet_id       = aws_subnet.file_format_private[1].id
-  security_groups = [aws_security_group.mount_target_sg[0].id]
-}
-
-resource "aws_security_group" "mount_target_sg" {
-  count       = local.count_file_format
-  name        = "mount-target-security-group"
-  description = "Mount target security group"
-  vpc_id      = data.aws_vpc.current.id
-
-  ingress {
-    description     = "EFS"
-    from_port       = 2049
-    to_port         = 2049
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_run_efs[count.index].id, aws_security_group.allow_efs_lambda[count.index].id]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    var.common_tags,
-    map("Name", "mount-target-outbound-only")
-  )
-}
-
-resource "aws_security_group" "ecs_run_efs" {
-  count       = local.count_file_format
-  name        = "allow-ecs-mount-efs"
-  description = "Allow ECS to mount EFS volume"
-  vpc_id      = data.aws_vpc.current.id
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    var.common_tags,
-    map("Name", "allow-ecs-mount-efs")
-  )
-}
-
-resource "aws_security_group" "allow_efs_lambda" {
-  count       = local.count_file_format
-  name        = "allow-efs"
-  description = "Allow EFS inbound traffic"
-  vpc_id      = data.aws_vpc.current.id
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    var.common_tags,
-    map("Name", "${var.project}-file-format-lambda-allow-efs")
-  )
 }
 
 resource "aws_lambda_event_source_mapping" "file_format_sqs_queue_mapping" {
@@ -130,7 +45,7 @@ resource "aws_cloudwatch_log_group" "file_format_lambda_log_group" {
 
 resource "aws_iam_policy" "file_format_lambda_policy" {
   count  = local.count_file_format
-  policy = templatefile("${path.module}/templates/file_format_lambda.json.tpl", { environment = local.environment, account_id = data.aws_caller_identity.current.account_id, update_queue = local.api_update_queue, input_sqs_queue = local.file_format_queue, file_system_id = var.file_system.id })
+  policy = templatefile("${path.module}/templates/file_format_lambda.json.tpl", { environment = local.environment, account_id = data.aws_caller_identity.current.account_id, update_queue = local.api_update_queue, input_sqs_queue = local.file_format_queue, file_system_id = var.file_system_id })
   name   = "${upper(var.project)}FileFormatLambdaPolicy${title(local.environment)}"
 }
 
@@ -144,38 +59,4 @@ resource "aws_iam_role_policy_attachment" "file_format_lambda_role_policy" {
   count      = local.count_file_format
   policy_arn = aws_iam_policy.file_format_lambda_policy.*.arn[0]
   role       = aws_iam_role.file_format_lambda_iam_role.*.name[0]
-}
-
-resource "aws_subnet" "file_format_private" {
-  count             = local.count_file_format_subnets
-  cidr_block        = cidrsubnet(data.aws_vpc.current.cidr_block, 6, count.index + 4)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = data.aws_vpc.current.id
-
-  tags = merge(
-    var.common_tags,
-    map("Name", "tdr-file-format-private-subnet-${count.index}-${local.environment}")
-  )
-}
-
-resource "aws_route_table" "file_format_private" {
-  count  = local.count_file_format_subnets
-  vpc_id = var.vpc_id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = [data.aws_nat_gateway.main_zero.id, data.aws_nat_gateway.main_one.id][count.index]
-  }
-
-  tags = merge(
-    var.common_tags,
-    map("Name", "route-table-${count.index}-tdr-${local.environment}")
-  )
-}
-
-# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
-resource "aws_route_table_association" "private" {
-  count          = local.count_file_format_subnets
-  subnet_id      = aws_subnet.file_format_private.*.id[count.index]
-  route_table_id = aws_route_table.file_format_private.*.id[count.index]
 }
