3db4a39dedf1ace0c1619126f734e7b0f863e965
TomJKing <tom.king@nationalarchives.gov.uk>
2020-10-02 14:51:09 +0100

    Add root directory env variable

--- lambda/yara_av.tf
+++ lambda/yara_av.tf
@@ -11,8 +11,9 @@ resource "aws_lambda_function" "lambda_function" {
   tags          = var.common_tags
   environment {
     variables = {
-      ENVIRONMENT = local.environment
-      SQS_URL     = "https://sqs.${var.region}.amazonaws.com/${data.aws_caller_identity.current.account_id}/${local.api_update_queue_name}"
+      ENVIRONMENT    = local.environment
+      ROOT_DIRECTORY = var.backend_checks_efs_root_directory_path
+      SQS_URL        = "https://sqs.${var.region}.amazonaws.com/${data.aws_caller_identity.current.account_id}/${local.api_update_queue_name}"
     }
   }
 }
