c58c35b0b4163198e4f20f909938731ace97b3b8
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-07 09:19:38 +0100

    Create separate variables for event targets I needed two event targets on the same cloudwatch rule. I tried to use the for_each blocks but this didn't work because the for_each was dependent on the lambda which didn't exist yet so this would stop us creating an environment in one command. We can add other conditional event targets if we need them to go to SQS queues.

--- cloudwatch_events/main.tf
+++ cloudwatch_events/main.tf
@@ -4,7 +4,14 @@ resource "aws_cloudwatch_event_rule" "ecr_image_scan" {
   event_pattern = templatefile("${path.module}/templates/${var.event_pattern}_pattern.json.tpl", {})
 }
 
-resource "aws_cloudwatch_event_target" "event_target" {
+resource "aws_cloudwatch_event_target" "sqs_event_target" {
+  count = local.count_sqs_event_target
   rule = aws_cloudwatch_event_rule.ecr_image_scan.name
-  arn  = var.event_target_arn
+  arn  = var.log_group_event_target_arn
+}
+
+resource "aws_cloudwatch_event_target" "lambda_event_target" {
+  count = local.count_lambda_event_target
+  rule = aws_cloudwatch_event_rule.ecr_image_scan.name
+  arn  = var.lambda_event_target_arn[count.index]
 }
