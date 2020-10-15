528d9c253ec06e68272e6b2913ddcb879a341a50
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-15 12:44:41 +0100

    Add event for maintenance window

--- cloudwatch_events/main.tf
+++ cloudwatch_events/main.tf
@@ -1,17 +1,17 @@
-resource "aws_cloudwatch_event_rule" "ecr_image_scan" {
-  name          = "ecr-image-scan"
-  description   = "Capture each ECR Image Scan"
-  event_pattern = templatefile("${path.module}/templates/${var.event_pattern}_pattern.json.tpl", {})
+resource "aws_cloudwatch_event_rule" "event_rule" {
+  name          = var.rule_name
+  description   = var.rule_description
+  event_pattern = templatefile("${path.module}/templates/${var.event_pattern}_pattern.json.tpl", var.event_variables)
 }
 
 resource "aws_cloudwatch_event_target" "sqs_event_target" {
   count = local.count_sqs_event_target
-  rule  = aws_cloudwatch_event_rule.ecr_image_scan.name
+  rule  = aws_cloudwatch_event_rule.event_rule.name
   arn   = var.log_group_event_target_arn
 }
 
 resource "aws_cloudwatch_event_target" "lambda_event_target" {
   count = local.count_lambda_event_target
-  rule  = aws_cloudwatch_event_rule.ecr_image_scan.name
+  rule  = aws_cloudwatch_event_rule.event_rule.name
   arn   = var.lambda_event_target_arn[count.index]
 }
---
+++ cloudwatch_events/templates/jenkins_maintenance_event_window_pattern.json.tpl
@@ -0,0 +1,16 @@
+{
+  "source": [
+    "aws.ssm"
+  ],
+  "detail-type": [
+    "Maintenance Window Task Execution State-change Notification"
+  ],
+  "detail": {
+    "status": [
+      "FAILED"
+    ],
+    "window-id": [
+      "${window_id}"
+    ]
+  }
+}
