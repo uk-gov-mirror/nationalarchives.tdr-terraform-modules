3941bcb5f823c11de2b03c160e93535c7e7fe5a8
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-06 12:48:20 +0000

    Add lambda policy and env vars

--- lambda/export_api_authoriser.tf
+++ lambda/export_api_authoriser.tf
@@ -1,13 +1,18 @@
 resource "aws_lambda_function" "export_api_authoriser_lambda_function" {
   count         = local.count_export_api_authoriser
   function_name = "${var.project}-export-api-authoriser-${local.environment}"
-  handler       = "uk.gov.nationalarchives.exportauthoriser.Lambda::process"
+  handler       = "uk.gov.nationalarchives.consignmentexport.authoriser.Lambda::process"
   role          = aws_iam_role.export_api_authoriser_lambda_iam_role.*.arn[0]
   runtime       = "java11"
   filename      = "${path.module}/functions/export-authoriser.jar"
   timeout       = 10
   memory_size   = 1024
   tags          = var.common_tags
+  environment {
+    variables = {
+      API_URL       = "${var.api_url}/graphql"
+    }
+  }
 
   lifecycle {
     ignore_changes = [filename]
@@ -26,6 +31,18 @@ resource "aws_iam_role" "export_api_authoriser_lambda_iam_role" {
   name               = "${upper(var.project)}ExportApiAuthoriserLambdaRole${title(local.environment)}"
 }
 
+resource "aws_iam_policy" "export_authoriser_policy" {
+  count = local.count_export_api_authoriser
+  name               = "${upper(var.project)}ExportApiAuthoriserLambdaPolicy${title(local.environment)}"
+  policy = templatefile("${path.module}/templates/export_authoriser_policy.json.tpl", {account_id = data.aws_caller_identity.current.account_id, environment = local.environment })
+}
+
+resource "aws_iam_role_policy_attachment" "export_authoriser_attachment" {
+  count = local.count_export_api_authoriser
+  policy_arn = aws_iam_policy.export_authoriser_policy[count.index].arn
+  role = aws_iam_role.export_api_authoriser_lambda_iam_role[count.index].id
+}
+
 data "aws_api_gateway_rest_api" "export_rest_api" {
   count = local.count_export_api_authoriser
   name  = "ExportAPI"
@@ -37,5 +54,5 @@ resource "aws_lambda_permission" "export_api_lambda_permissions" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.export_api_authoriser_lambda_function.*.arn[count.index]
   principal     = "apigateway.amazonaws.com"
-  source_arn    = data.aws_api_gateway_rest_api.export_rest_api.*.arn[count.index]
+  source_arn    = "${data.aws_api_gateway_rest_api.export_rest_api.*.execution_arn[count.index]}/authorizers/*"
 }
