be1f3c6735822b0b646ee83225fa8a5780656a9f
Sam Palmer <github@sampalmer.dev>
2021-03-03 22:33:27 +0000

    Put the depends_on the correct way round

--- s3/main.tf
+++ s3/main.tf
@@ -32,13 +32,13 @@ resource "aws_s3_bucket_public_access_block" "log_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  depends_on = [aws_s3_bucket_policy.log_bucket]
 }
 
 resource "aws_s3_bucket_policy" "log_bucket" {
   count  = var.access_logs == true && var.apply_resource == true ? 1 : 0
   bucket = aws_s3_bucket.log_bucket.*.id[0]
   policy = templatefile("./tdr-terraform-modules/s3/templates/secure_transport.json.tpl", { bucket_name = aws_s3_bucket.log_bucket.*.id[0] })
+  depends_on = [aws_s3_bucket_public_access_block.log_bucket]
 }
 
 resource "aws_s3_bucket_notification" "log_bucket_notification" {
@@ -123,6 +123,7 @@ resource "aws_s3_bucket_policy" "bucket" {
   count  = var.apply_resource == true ? 1 : 0
   bucket = aws_s3_bucket.bucket.*.id[0]
   policy = local.environment == "mgmt" && contains(["log-data", "lambda_update"], var.bucket_policy) ? templatefile("./tdr-terraform-modules/s3/templates/${var.bucket_policy}.json.tpl", { bucket_name = aws_s3_bucket.bucket.*.id[0], account_id = data.aws_caller_identity.current.account_id, external_account_1 = data.aws_ssm_parameter.intg_account_number.*.value[0], external_account_2 = data.aws_ssm_parameter.staging_account_number.*.value[0], external_account_3 = data.aws_ssm_parameter.prod_account_number.*.value[0] }) : templatefile("./tdr-terraform-modules/s3/templates/${var.bucket_policy}.json.tpl", { bucket_name = aws_s3_bucket.bucket.*.id[0], aws_elb_account = data.aws_ssm_parameter.aws_elb_account_number.value })
+  depends_on = [aws_s3_bucket_public_access_block.bucket]
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
@@ -132,7 +133,6 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
-  depends_on              = [aws_s3_bucket_policy.bucket]
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
