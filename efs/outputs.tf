1ddafc53ce6f314440f64f968e6b8a3ce4be94e2
Sam Palmer <github@sampalmer.dev>
2021-03-03 22:32:16 +0000

    Output the mount targets which are used by the lambdas

--- efs/outputs.tf
+++ efs/outputs.tf
@@ -14,3 +14,11 @@ output "private_subnets" {
   value = aws_subnet.efs_private.*.id
 }
 
+output "mount_target_zero" {
+  value = aws_efs_mount_target.mount_target_az_zero
+}
+
+output "mount_target_one" {
+  value = aws_efs_mount_target.mount_target_az_one
+}
+
