a4993a48383696e309904a14a099a6724230319f
Sam Palmer <github@sampalmer.dev>
2021-03-03 21:00:29 +0000

    Wait for the bucket policy to create before setting public access block
    This sometimes worked before and sometimes gave an error about not being able to update because updates were in progress. This stops those.

--- s3/main.tf
+++ s3/main.tf
@@ -32,6 +32,7 @@ resource "aws_s3_bucket_public_access_block" "log_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+  depends_on = [aws_s3_bucket_policy.log_bucket]
 }
 
 resource "aws_s3_bucket_policy" "log_bucket" {
@@ -48,6 +49,7 @@ resource "aws_s3_bucket_notification" "log_bucket_notification" {
     topic_arn = local.log_data_sns_topic_arn
     events    = ["s3:ObjectCreated:*"]
   }
+  depends_on = [aws_s3_bucket_policy.log_bucket]
 }
 
 resource "aws_s3_bucket" "bucket" {
