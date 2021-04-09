698456e76560e932c04aadd421c00ffef34d2f00
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-09 09:47:12 +0100

    Add line breaks and remove temporary code

--- ses/data.tf
+++ ses/data.tf
@@ -1,3 +1,3 @@
-//data "aws_ssm_parameter" "notification_email_prefix" {
-//  name = "/mgmt/notification/email/prefix"
-//}
+data "aws_ssm_parameter" "notification_email_prefix" {
+  name = "/mgmt/notification/email/prefix"
+}
