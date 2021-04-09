859a8ff7ff443fd7804964fbeef43c2f36e9102c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-09 09:59:30 +0100

    Ran terraform fmt

--- kms/main.tf
+++ kms/main.tf
@@ -1,8 +1,8 @@
 data "template_file" "key_policy" {
   template = file("./tdr-terraform-modules/kms/templates/${var.key_policy}.json.tpl")
   vars = {
-    account_id      = data.aws_caller_identity.current.account_id
-    environment     = var.environment
+    account_id  = data.aws_caller_identity.current.account_id
+    environment = var.environment
   }
 }
 
